bafb6e20b303c2f44c85f9bfacdbcf42e924f2b1
Igor Costa Carvalho Campos Silva <igorsilvabhz6@gmail.com>
2021-04-28 18:30:30 -0300

    Separa adição de matrizes

--- src/matrizes.py
+++ src/matrizes.py
@@ -23,21 +23,6 @@ C = [
     [0, 0, 0, 0]
 ]
 
-print("A + B = C")
-j = 0
-i = 0
-while(i < 4):
-    if j < 4:
-        C[i][j] = A[i][j] + B[i][j]
-        print(f'{C[i][j]}\t', end = "")
-        j = j + 1
-    else:
-        print("")
-        i = i + 1
-        j = 0
-
-
-
 
 
 print("a*A = C")
