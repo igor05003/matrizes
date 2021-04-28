fb4ed45eb70a9eed2fdb9d3c992882f6b71781c4
Igor Costa Carvalho Campos Silva <igorsilvabhz6@gmail.com>
2021-04-28 18:33:46 -0300

    Separa multplicação da matriz A por um número

--- src/matrizes.py
+++ src/matrizes.py
@@ -25,35 +25,6 @@ C = [
 
 
 
-print("a*A = C")
-a = float(input("a = " ))
-
-i = 0
-while(i < 4):
-    j = 0
-    while(j < 4):
-        C[i][j] = A[i][j] * a
-        j = j + 1
-
-    i = i + 1
-
-
-i = 0
-while(i < 4):
-    j = 0
-    while(j < 4):
-        print(f'{C[i][j]}\t', end = "")
-        j = j + 1
-
-    print("")
-    i = i + 1
-
-
-
-
-
-
-
 
 print("A")
 
