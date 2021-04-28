ced2f366724d055fbcc58528a7799f2c5231c79b
Igor Costa Carvalho Campos Silva <igorsilvabhz6@gmail.com>
2021-04-28 18:37:24 -0300

    Separa a Matriz A triangular superior

--- src/Matriz A triangular superior.py
+++ src/Matriz A triangular superior.py
@@ -24,6 +24,25 @@ C = [
 ]
 
 
+
+
+
+print("A triangular superior")
+
+i = 0
+j = 0
+while(i < 4):
+    if j < 4:
+        print(f'{A[i][j]}\t', end = "")
+        j = j + 1
+    else:
+        print("")
+        i = i + 1
+        j = i
+
+
+
+
 print("A triangular inferior")
 
 i = 0
