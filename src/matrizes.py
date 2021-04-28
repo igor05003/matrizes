fe61c695068ca2938b3b6c7f3e077c99d6261b5e
Igor Costa Carvalho Campos Silva <igorsilvabhz6@gmail.com>
2021-04-28 18:25:43 -0300

    adiciona funções primitivas de matrizes

---
+++ src/matrizes.py
@@ -0,0 +1,127 @@
+import sys
+import os
+import string
+
+A = [
+    [1, 2, 3, 4],
+    [1, 2, 3, 4],
+    [1, 2, 3, 4],
+    [1, 2, 3, 4]
+]
+
+B = [
+    [1, 2, 3, 4],
+    [1, 2, 3, 4],
+    [1, 2, 3, 4],
+    [1, 2, 3, 4]
+]
+
+C = [
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0]
+]
+
+print("A + B = C")
+j = 0
+i = 0
+while(i < 4):
+    if j < 4:
+        C[i][j] = A[i][j] + B[i][j]
+        print(f'{C[i][j]}\t', end = "")
+        j = j + 1
+    else:
+        print("")
+        i = i + 1
+        j = 0
+
+
+
+
+
+print("a*A = C")
+a = float(input("a = " ))
+
+i = 0
+while(i < 4):
+    j = 0
+    while(j < 4):
+        C[i][j] = A[i][j] * a
+        j = j + 1
+
+    i = i + 1
+
+
+i = 0
+while(i < 4):
+    j = 0
+    while(j < 4):
+        print(f'{C[i][j]}\t', end = "")
+        j = j + 1
+
+    print("")
+    i = i + 1
+
+
+
+
+
+
+
+
+print("A")
+
+i = 0
+j = 0
+while(i < 4):
+    if j < 4:
+        print(f'{A[i][j]}\t', end = "")
+        j = j + 1
+    else:
+        print("")
+        i = i + 1
+        j = 0
+
+
+
+
+
+
+
+print("A triangular superior")
+
+i = 0
+j = 0
+while(i < 4):
+    if j < 4:
+        print(f'{A[i][j]}\t', end = "")
+        j = j + 1
+    else:
+        print("")
+        i = i + 1
+        j = i
+
+
+
+
+print("A triangular inferior")
+
+i = 0
+j = 0
+while(i < 4):
+    if j == i:
+        print(f'{A[i][j]}\t', end = "")
+        i = i + 1
+        j = 0
+        print("")
+    else:
+        print(f'{A[i][j]}\t', end = "")
+        j = j + 1
+
+print("A * B = C")                                      # Aij = Bji
+
+i = 0
+while(i < 4):
+    if i == j:
+        C[i][j] = A[i] * B[j]
---
+++ src/multiplicação.py
@@ -0,0 +1,64 @@
+import sys
+import os
+import string
+
+A = [
+    [1, 2, 3, 4],
+    [1, 2, 3, 4],
+    [1, 2, 3, 4],
+    [1, 2, 3, 4]
+]
+
+B = [
+    [1, 2, 3, 4],
+    [1, 2, 3, 4],
+    [1, 2, 3, 4],
+    [1, 2, 3, 4]
+]
+
+C = [
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0]
+]
+
+print("A * B = C")
+print(len(A[0]))
+print(len(B))
+if (len(A[0])) != (len(B)):
+    print("Erro! Esta multiplicação não pode ser calculada")
+else:
+    i = 0
+    while(i < 4):
+        j = 0
+        while(j < 4):
+            summ = 0
+            k = 0
+            while(k < 4):
+                summ = summ + A[i][k] * B[k][j]
+                k = k + 1
+            C[i][j] = summ
+            j = j + 1
+        i = i + 1
+
+
+
+
+
+
+i = 0
+while(i < 4):
+    j = 0
+    while(j < 4):
+        print(f'{C[i][j]}\t', end = "")
+        j = j + 1
+
+    print("")
+    i = i + 1
+
+
+
+
+
+    
