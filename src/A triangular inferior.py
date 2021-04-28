1aa03bae9dcdc84f5c0d969f963a3f7809a5c992
Igor Costa Carvalho Campos Silva <igorsilvabhz6@gmail.com>
2021-04-28 18:39:52 -0300

    Separa a Matriz A triangular inferior

--- src/A triangular inferior.py
+++ src/A triangular inferior.py
@@ -22,3 +22,18 @@ C = [
     [0, 0, 0, 0],
     [0, 0, 0, 0]
 ]
+
+
+print("A triangular inferior")
+
+i = 0
+j = 0
+while(i < 4):
+    if j == i:
+        print(f'{A[i][j]}\t', end = "")
+        i = i + 1
+        j = 0
+        print("")
+    else:
+        print(f'{A[i][j]}\t', end = "")
+        j = j + 1
