48d3a10a2ed4dca8422c143cf767b7471029d5ad
Igor Costa Carvalho Campos Silva <igorsilvabhz6@gmail.com>
2021-05-01 02:33:58 -0300

    Corrige multiplicador

--- src/multiplicador.py
+++ src/multiplicador.py
@@ -4,6 +4,19 @@ def Multiplicar(A, B):
     if (len(A[0])) != (len(B)):
         print("Erro! Esta multiplicação não pode ser calculada")
     else:
+        C = []
+        i = 0
+        while(i < len(A)):
+            line = []
+            j = 0
+            while(j < len(B[0])):
+                line.append(0)
+                j = j + 1
+
+            C.append(line)
+            i = i + 1
+
+
         i = 0
         while(i < 4):
             j = 0
@@ -13,8 +26,8 @@ def Multiplicar(A, B):
                 while(k < 4):
                     summ = summ + A[i][k] * B[k][j]
                     k = k + 1
-                A[i][j] = summ
+                C[i][j] = summ
                 j = j + 1
             i = i + 1
 
-    return A
+    return C
